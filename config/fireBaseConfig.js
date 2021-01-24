7bf5a57f7a6fc29b1d6e99058246cb759a3fc3cd
MartinGonzalez100 <martineg100@gmail.com>
2021-01-24 04:29:26 -0300

    trabajando con formularios de inicio de sesion

---
+++ config/fireBaseConfig.js
@@ -0,0 +1,31 @@
+
+
+
+  // Your web app's Firebase configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyAn8mF8akEqrB2GajlvfW0N67pFMfv4bfA",
+//     authDomain: "proyectofinal-95c98.firebaseapp.com",
+//     projectId: "proyectofinal-95c98",
+//     storageBucket: "proyectofinal-95c98.appspot.com",
+//     messagingSenderId: "342957279584",
+//     appId: "1:342957279584:web:9f1f27bb08ca5422de3605"
+//   };
+//base prueba
+// Your web app's Firebase configuration
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDEHn6TxLcKUoJ-n9362w0HNaVBTuffpe0",
+//     authDomain: "proyectofinal-9b976.firebaseapp.com",
+//     projectId: "proyectofinal-9b976",
+//     storageBucket: "proyectofinal-9b976.appspot.com",
+//     messagingSenderId: "935267993825",
+//     appId: "1:935267993825:web:57e3a7957dcbe64bd60fa0"
+//   };
+
+
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+//   module.exports = {
+//     firebase
+//   }
---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+
+
+// const {registroUsuario, logueoUsuario} = require("../../controllers/loginControllers")
+
+// //funcionalidad del login
+
+// const botonDeRegistro = document.querySelector("#botonDeRegistro")
+// const  campoEmail = document.querySelector("#staticEmail2")
+// const  campoPassword = document.querySelector("#inputPassword2")
+
+
+// botonDeRegistro.addEventListener("click",(e)=>{
+//     e.preventDefault()
+//     alert("#hola mundo")
+//     console.log(campoEmail.value,campoPassword.value)
+//     registroUsuario(campoEmail.value, campoPassword.value)
+//     // logueoUsuario(campoEmail.value, campoPassword.value)
+// })
