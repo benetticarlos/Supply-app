8ce32a91caac8cfc68f6e9019e83a22ec0fd99e8
Brenda <brendamailenibanez23@gmail.com>
2021-02-02 11:23:02 -0300

    base 2

--- controllers/publicacionControlles.js
+++ controllers/publicacionControlles.js
@@ -1,136 +1,84 @@
 
 const conexion_db = require('../config/dbConfig')
 
-const obtenerPublicacion = (req, res)=>{
-    conexion_db.query('select * from mipublicacion_t', (err, result)=>{
-        if(err){
-            throw err
+// const obtenerPublicacion = (req, res)=>{
+//     conexion_db.query('select * from mipublicacion_t', (err, result)=>{
+//         if(err){
+//             throw err
         
-        }else {           
+//         }else {           
            
-                console.log(result)
-                res.send(result)
-        }
-    })
-}
+//                 console.log(result)
+//                 res.send(result)
+//         }
+//     })
+// }
 
-const agregarPublicacion = (req, res)=>{
-   console.log(req.body)
-   const {id, publicacion, detalle} = req.body
-    conexion_db.query('INSERT INTO `mipublicacion_t`(`id`, `publicacion`, `detalle`) VALUES (?,?,?)', [id, publicacion, detalle],(err, result)=>{
-        if(err){
-            throw err
+// const agregarPublicacion = (req, res)=>{
+//    console.log(req.body)
+//    const {id, publicacion, detalle} = req.body
+//     conexion_db.query('INSERT INTO `mipublicacion_t`(`id`, `publicacion`, `detalle`) VALUES (?,?,?)', [id, publicacion, detalle],(err, result)=>{
+//         if(err){
+//             throw err
         
-        }else {           
+//         }else {           
            
-                console.log(result)
-            }
-        })
-        res.send("alta exitosa")
-}
+//                 console.log(result)
+//             }
+//         })
+//         res.send("alta exitosa")
+// }
 
-const borrarPublicacion = (req, res) => {
-    const {id} = req.body
-    conexion_db.query('DELETE FROM `mipublicacion_t` WHERE id = ?',[id] ,(err, result) =>{
-if(err){
-    throw err
-}else{
-    console.log(result)
-    res.send('elemento borrado')
-}
-    })
-    console.log('elemento borrado')
-}
+// const borrarPublicacion = (req, res) => {
+//     const {id} = req.body
+//     conexion_db.query('DELETE FROM `mipublicacion_t` WHERE id = ?',[id] ,(err, result) =>{
+// if(err){
+//     throw err
+// }else{
+//     console.log(result)
+//     res.send('elemento borrado')
+// }
+//     })
+//     console.log('elemento borrado')
+// }
 
-const editarPublicacion = (req, res) =>{
-    const {id, publicacion, detalle} = req.body
-    conexion_db.query('UPDATE `mipublicacion_t` SET `publicacion`=?,`detalle`=? WHERE id = ?',[publicacion, detalle, id],(err, result) =>{
-        if(err){
-            throw err
-        }else{
-            console.log(result)
-            res.send('elemento editado')
-        }
-            })
-            console.log('elemento editado')
-        }
+// const editarPublicacion = (req, res) =>{
+//     const {id, publicacion, detalle} = req.body
+//     conexion_db.query('UPDATE `mipublicacion_t` SET `publicacion`=?,`detalle`=? WHERE id = ?',[publicacion, detalle, id],(err, result) =>{
+//         if(err){
+//             throw err
+//         }else{
+//             console.log(result)
+//             res.send('elemento editado')
+//         }
+//             })
+//             console.log('elemento editado')
+//         }
 
 
 
   
-module.exports = {
-                    obtenerPublicacion,
-                    agregarPublicacion,
-                    borrarPublicacion,
-                    editarPublicacion
-                }
+// module.exports = {
+//                     obtenerPublicacion,
+//                     agregarPublicacion,
+//                     borrarPublicacion,
+//                     editarPublicacion
+//                 }
 
 
 
-const obtenerPublicacion = (req, res)=>{
-    conexion_db.query('select * from publicacion_t', (err, result)=>{
-        if(err){
-            throw err
+// const obtenerPublicacion = (req, res)=>{
+//     conexion_db.query('select * from publicacion_t', (err, result)=>{
+//         if(err){
+//             throw err
         
-        }else {           
+//         }else {           
            
-                console.log(result)
-                res.send(result)
-        }
-    })
-}
-
-<<<<<<< HEAD
-const agregarPublicacion = (req, res)=>{
-   console.log(req.body)
-   const {id, publicacion, detalle, precio, zona, imagenurl, rubro, condicion, idusuario} = req.body
-    conexion_db.query('INSERT INTO `publicacion_t`(`id`, `publicacion`, `detalle`, `precio`, `zona`, `imagenurl`, `rubro`, `condicion`, `idusuario`) VALUES (?,?,?,?,?)', [id, publicacion, detalle, precio, zona, imagenurl, rubro, condicion, idusuario],(err, result)=>{
-        if(err){
-            throw err
-        
-        }else {           
-           
-                console.log(result)
-            }
-        })
-        res.send("alta exitosa")
-}
-
-const borrarPublicacion = (req, res) => {
-    const {id} = req.body
-    conexion_db.query('DELETE FROM `publicacion_t` WHERE id = ?',[id] ,(err, result) =>{
-if(err){
-    throw err
-}else{
-    console.log(result)
-    res.send('elemento borrado')
-}
-    })
-    console.log('elemento borrado')
-}
-
-const editarPublicacion = (req, res) =>{
-    const {id, publicacion, detalle} = req.body
-    conexion_db.query('UPDATE `publicacion_t` SET `publicacion`=?,`detalle`=? WHERE id = ?',[publicacion, detalle, id],(err, result) =>{
-        if(err){
-            throw err
-        }else{
-            console.log(result)
-            res.send('elemento editado')
-        }
-            })
-            console.log('elemento editado')
-        }
-
-
-
-  
-module.exports = {
-                    obtenerPublicacion,
-                    agregarPublicacion,
-                    borrarPublicacion,
-                    editarPublicacion
-                }
+//                 console.log(result)
+//                 res.send(result)
+//         }
+//     })
+// }
 
 
 const obtenerPublicacion = (req, res)=>{
@@ -186,17 +134,6 @@ const editarPublicacion = (req, res) =>{
             })
             console.log('elemento editado')
         }
-
-
-  
-module.exports = {
-                    obtenerPublicacion,
-                    agregarPublicacion,
-                    borrarPublicacion,
-                    editarPublicacion
-                }
-
-
 
 
 
@@ -258,15 +195,14 @@ const agregarComentario = (req, res)=>{
                }
                 
                 
-                
-                  
-  module.exports = {
-                     obtenerComentario,
-                     agregarComentario,
-                     borrarComentario,
-                     editarComentario
-                                }
-=======
-
-              
->>>>>>> 99cdf22945337ccc357955d53cc1b9a04de50171
+module.exports = {
+                obtenerPublicacion,
+                agregarPublicacion,
+                borrarPublicacion,
+                editarPublicacion,
+                obtenerComentario,
+                 agregarComentario,
+                 borrarComentario,
+                 editarComentario
+            }
+  
