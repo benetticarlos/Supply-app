1f2c4f32c6e67d5ff7a9a14dec69a4ac8d99bf74
CarlosBenetti <benetticarlos1@gmail.com>
2021-02-05 06:59:19 -0300

    restauracion antes del pull

--- public/js/cargarImagen.js
+++ public/js/cargarImagen.js
@@ -21,7 +21,7 @@ upload.addEventListener('change', function (e) {
 
 
     //crear referencia storage
-    var storageRef = firebase.storage().ref('imagenes/' + archivo.name)
+    var storageRef = firebase.storage().ref('imagenes/' + "imagen")
 
     // subir archivo
     storageRef.put(archivo)
@@ -31,10 +31,15 @@ upload.addEventListener('change', function (e) {
 
 
 
+
+
+
+
+
 // window.onload = inicializar
 
 // var upload;
-// // var storageRef
+// var storageRef
 
 // function inicializar() {
 //     upload = document.getElementById('upload')
@@ -47,6 +52,28 @@ upload.addEventListener('change', function (e) {
 // function subirImagenAFirebase() {
 //     var imagenASubir = upload.files[0];
 
-//     var uploadTask = storageRef.put(imagenASubir)
+//     var uploadTask = storageRef.child('imagenes/' + imagenASubir.name).put(imagenASubir);
 
+  
+//     uploadTask.on('state_changed', 
+//     function(snapshot){
+//       // se va mostrando el progreso de la subida
+//       var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+//       console.log('Upload is ' + progress + '% done');
+//       switch (snapshot.state) {
+//         case firebase.storage.TaskState.PAUSED: // or 'paused'
+//           console.log('Upload is paused');
+//           break;
+//         case firebase.storage.TaskState.RUNNING: // or 'running'
+//           console.log('Upload is running');
+//           break;
+//       }
+//     }, function(error) {
+//       // gestionar el error si se produce
+//     }, function() {
+//       //cuando se ha subido exitosamente la imagen
+//       uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+//         console.log('File available at', downloadURL);
+//       });
+//     });
 // }
