0bc1201d90ab574fd977af2b37b8977c017a1950
CarlosBenetti <benetticarlos1@gmail.com>
2021-02-09 17:36:38 -0300

    firebase andando y comentarios

--- public/js/cargarServicio.js
+++ public/js/cargarServicio.js
@@ -1,270 +1,144 @@
-
-          // Your web app's Firebase configuration
-          var firebaseConfig = {
-            apiKey: "AIzaSyCmRFONWKjnYGHrny9AqeXqJS7FxfC4R9E",
-            authDomain: "localstorage-e6721.firebaseapp.com",
-            projectId: "localstorage-e6721",
-            storageBucket: "localstorage-e6721.appspot.com",
-            messagingSenderId: "719797837765",
-            appId: "1:719797837765:web:6cf7cce3a249d07edbf5ac"
-          };
-          // Initialize Firebase
-          firebase.initializeApp(firebaseConfig);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Codigo firebase Carlos
+var firebaseConfig = {
+    apiKey: "AIzaSyCmRFONWKjnYGHrny9AqeXqJS7FxfC4R9E",
+    authDomain: "localstorage-e6721.firebaseapp.com",
+    projectId: "localstorage-e6721",
+    storageBucket: "localstorage-e6721.appspot.com",
+    messagingSenderId: "719797837765",
+    appId: "1:719797837765:web:6cf7cce3a249d07edbf5ac",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 //traer el formulario
-let formularioServicio = document.getElementById('formularioServicio')
+let formularioServicio = document.getElementById("formularioServicio");
 
 //cuando aprete el botón enviar datos se ejecutara esta funcion
-formularioServicio.addEventListener('submit', function (e) {
+formularioServicio.addEventListener("submit", function (e) {
     e.preventDefault();
-    console.log('me diste un click');
+    console.log("me diste un click");
 
     //traigo los datos del formulario
-    let Datos = new FormData(formularioServicio)
-
+    let Datos = new FormData(formularioServicio);
 
     //funcion "precio" dependiendo si esta
     //presionado el check o no
-    let precio = ""
-    let precioCheck = document.getElementById("acordarVendedor")
+    let precio = "";
+    let precioCheck = document.getElementById("acordarVendedor");
 
     if (precioCheck.checked) {
-        precio = "a acordar con el vendedor"
+        precio = "a acordar con el vendedor";
     } else {
-        precio = Datos.get('precio')
+        precio = Datos.get("precio");
     }
 
     console.log("este es mi ", precio);
 
     //tomar valores del select
-    let SelectRubros = document.getElementById('rubros')
-    let rubro = SelectRubros.value
+    let SelectRubros = document.getElementById("rubros");
+    let rubro = SelectRubros.value;
 
     // consologeamos los valores
     console.log(Datos);
-    console.log("este es mi publicacion: ", Datos.get('publicacion'));
+    console.log("este es mi publicacion: ", Datos.get("publicacion"));
     console.log("este es mi rubro: ", rubro);
-    console.log("este es mi precio: ", Datos.get('precio'));
-    console.log("este es mi zona: ", Datos.get('zona'));
-    console.log("este es mi detalle: ", Datos.get('detalle'));
+    console.log("este es mi precio: ", Datos.get("precio"));
+    console.log("este es mi zona: ", Datos.get("zona"));
+    console.log("este es mi detalle: ", Datos.get("detalle"));
     console.log("esto es mi blob ", blobImagen);
 
     //Recojo los datos para enviarlos al servidor
     let _data = {
-        "publicacion": Datos.get('publicacion'),
-        "detalle": Datos.get('detalle'),
-        "precio": precio,
-        "zona": Datos.get('zona'),
-        "imagenurl": srcImagen,
-        "rubro": rubro,
-        "condicion": "",
-        "idusuario": ""
-    }
+        publicacion: Datos.get("publicacion"),
+        detalle: Datos.get("detalle"),
+        precio: precio,
+        zona: Datos.get("zona"),
+        imagenurl: srcImagen,
+        rubro: rubro,
+        condicion: "",
+        idusuario: "",
+    };
 
-    fetch('http://localhost:8080/ingresarproducto', {
+    fetch("http://localhost:8080/ingresarproducto", {
         method: "POST",
         body: JSON.stringify(_data),
-        headers: { "Content-type": "application/json; charset=UTF-8" }
+        headers: { "Content-type": "application/json; charset=UTF-8" },
     })
-        .then(response => response.json())
-        .then(json => console.log(json))
-        .catch(err => console.log(err))
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch((err) => console.log(err));
+}); //termina el addeventlistener
 
-}) //termina el addeventlistener
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ------------------FUNCION BLOB, ANDANDO PERO CON PROBLEMAS --------------------------------------
-
+// Cargado de la imagen
 
 //  Funcion que se ejecuta cuando se carga una imagen
-var blobImagen = ""
-imagenurl.addEventListener('change', function (e) {
+var blobImagen = "";
+imagenurl.addEventListener("change", function (e) {
     //  Recojo el archivo subid
-    archivo = e.target.files[0]
+    archivo = e.target.files[0];
     // lo convierte en BLOB
-    const imgCodified = URL.createObjectURL(archivo)
-    pruebasrc.src = imgCodified
+    const imgCodified = URL.createObjectURL(archivo);
+    pruebasrc.src = imgCodified;
     console.log(imgCodified);
-    blobImagen = imgCodified
+    blobImagen = imgCodified;
 
     //  Elimina el icono para que no se pise con la imagen
-    var padre = document.getElementById("IngresarImagenBoton")
-    var hijo = document.getElementById("iconoDescarga")
-    padre.removeChild(hijo)
-})
+    var padre = document.getElementById("IngresarImagenBoton");
+    var hijo = document.getElementById("iconoDescarga");
+    padre.removeChild(hijo);
+});
 
-
-// ---------------------------------------------------------------------------------------
-
-
-
-
-//------------------------FUNCION DE PRUEBA, FIREBASE ------------------------------------
-
+// FIREBASE
 
 function iniciar() {
+    // Consigo el ultimo ID
+    var ultimoId = "";
+    fetch("http://localhost:8080/ultimoId")
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((elemento) => {
+                elemento.UId;
+                maxId = elemento.UId;
+                console.log(maxId);
 
-    var ultimoId = ''
-    fetch('http://localhost:8080/ultimoId')
-    .then(response => response.json())
-    .then(data =>{
-        data.forEach(elemento =>{ elemento.UId
-            maxId = elemento.UId
-            console.log(maxId);
+                // Cambio el nombre de la imagen por el del ID + 1
+                // para ligarla con el formulario que sigue
+                nombreImagen = "imagen" + (maxId + 1);
+                console.log("este es el nombre: ", nombreImagen);
 
+                var imagenurl = document.getElementById("imagenurl");
 
-    nombreImagen = "imagen" + (maxId + 1)
+                imagenurl.addEventListener("change", function (e) {
+                    //tomar archivo
+                    var archivo = e.target.files[0];
 
-    console.log("este es el nombre: ",nombreImagen);
-    
-    
-    var imagenurl  = document.getElementById('imagenurl')
-    
-    imagenurl.addEventListener('change', function (e) {
-        
-        //tomar archivo
-        var archivo = e.target.files[0];
-        
+                    //crear referencia storage
+                    var storageRef = firebase.storage().ref("imagenes/" + nombreImagen);
 
-    //crear referencia storage
-    var storageRef = firebase.storage().ref('imagenes/' + nombreImagen)
-    
-    // subir archivo
-    storageRef.put(archivo)
-    
-    console.log("imagen subida a firebase");
-    
-    // var pruebasrc = document.getElementById("pruebassrc")
+                    // subir archivo
+                    storageRef.put(archivo);
 
-    srcImagen = `https://firebasestorage.googleapis.com/v0/b/localstorage-e6721.appspot.com/o/imagenes%2F${nombreImagen}?alt=media&token=9c172991-c595-4817-82a5-589496d554e1`
-    
-        console.log(pruebasrc);
-    
-})
-})
+                    console.log("imagen subida a firebase");
 
+                    // Cambia el src de la imagen en el documento
+                    // por la URL de la imagen en firebase
 
+                    srcImagen = `https://firebasestorage.googleapis.com/v0/b/localstorage-e6721.appspot.com/o/imagenes%2F${nombreImagen}?alt=media&token=9c172991-c595-4817-82a5-589496d554e1`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-    })
-
+                    console.log(pruebasrc);
+                });
+            });
+        });
 }
 
- window.onload = iniciar
-
-
-
-
-
-// ------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Inicia la funcion cuando carga la pagina
+window.onload = iniciar;
 
 //redireccionar a cuerpo
-const botonPublicarBusqueda = document.querySelector("#botonPublicarBusqueda")
+const botonPublicarBusqueda = document.querySelector("#botonPublicarBusqueda");
 
-botonPublicarBusqueda.addEventListener("click", ()=>{
-    setTimeout(()=>{
-        location.href = "http://localhost:8080/cuerpo.html"
-    }, 3000)
-})
-
-
+botonPublicarBusqueda.addEventListener("click", () => {
+    setTimeout(() => {
+        location.href = "http://localhost:8080/cuerpo.html";
+    }, 3000);
+});
--- public/js/volviendoProbarFirebase.js
+++ public/js/volviendoProbarFirebase.js
@@ -1,147 +1,52 @@
-          // Your web app's Firebase configuration
-          var firebaseConfig = {
-            apiKey: "AIzaSyCmRFONWKjnYGHrny9AqeXqJS7FxfC4R9E",
-            authDomain: "localstorage-e6721.firebaseapp.com",
-            projectId: "localstorage-e6721",
-            storageBucket: "localstorage-e6721.appspot.com",
-            messagingSenderId: "719797837765",
-            appId: "1:719797837765:web:6cf7cce3a249d07edbf5ac"
-          };
-          // Initialize Firebase
-          firebase.initializeApp(firebaseConfig);
-
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCmRFONWKjnYGHrny9AqeXqJS7FxfC4R9E",
+    authDomain: "localstorage-e6721.firebaseapp.com",
+    projectId: "localstorage-e6721",
+    storageBucket: "localstorage-e6721.appspot.com",
+    messagingSenderId: "719797837765",
+    appId: "1:719797837765:web:6cf7cce3a249d07edbf5ac",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function iniciar() {
+    var ultimoId = "";
+    fetch("http://localhost:8080/ultimoId")
+        .then((response) => response.json())
+        .then((data) => {
+            data.forEach((elemento) => {
+                elemento.UId;
+                maxId = elemento.UId;
+                console.log(maxId);
 
-    var ultimoId = ''
-    fetch('http://localhost:8080/ultimoId')
-    .then(response => response.json())
-    .then(data =>{
-        data.forEach(elemento =>{ elemento.UId
-            maxId = elemento.UId
-            console.log(maxId);
+                nombreImagen = "imagen" + (maxId + 1);
 
+                console.log("este es el nombre: ", nombreImagen);
 
-    nombreImagen = "imagen" + (maxId + 1)
+                var imagenurl = document.getElementById("imagenurl");
 
-    console.log("este es el nombre: ",nombreImagen);
-    
-    
-    var imagenurl  = document.getElementById('imagenurl')
-    
-    imagenurl.addEventListener('change', function (e) {
-        
-        //tomar archivo
-        var archivo = e.target.files[0];
-        
+                imagenurl.addEventListener("change", function (e) {
+                    //tomar archivo
+                    var archivo = e.target.files[0];
 
-    //crear referencia storage
-    var storageRef = firebase.storage().ref('imagenes/' + nombreImagen)
-    
-    // subir archivo
-    storageRef.put(archivo)
-    
-    console.log("imagen subida a firebase");
-    
-    var miImagen = document.getElementById("miImagen")
+                    //crear referencia storage
+                    var storageRef = firebase.storage().ref("imagenes/" + nombreImagen);
 
-                miImagen.src = `https://firebasestorage.googleapis.com/v0/b/localstorage-e6721.appspot.com/o/imagenes%2F${nombreImagen}?alt=media&token=9c172991-c595-4817-82a5-589496d554e1`
-    
-    
-})
-})
+                    // subir archivo
+                    storageRef.put(archivo);
 
+                    console.log("imagen subida a firebase");
 
+                    var miImagen = document.getElementById("miImagen");
 
-
-
-
-
-
-
-
-
-
-
-
-
-    })
-
+                    miImagen.src = `https://firebasestorage.googleapis.com/v0/b/localstorage-e6721.appspot.com/o/imagenes%2F${nombreImagen}?alt=media&token=9c172991-c595-4817-82a5-589496d554e1`;
+                });
+            });
+        });
 }
 
- window.onload = iniciar
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.onload = iniciar;
 
 // var imagenurl  = document.getElementById('imagenurl')
 
@@ -149,7 +54,6 @@ function iniciar() {
 
 //     //tomar archivo
 //     var archivo = e.target.files[0];
-
 
 //     //crear referencia storage
 //     var storageRef = firebase.storage().ref('imagenes/' + archivo.name)
@@ -159,15 +63,11 @@ function iniciar() {
 
 //     console.log("imagen subida a firebase");
 
-
-
-
 // })
 
 // var boton = document.getElementById('boton')
 
 // boton.addEventListener('click', function () {
-    
 
 // //-----------------Funcion que encuentra el ultimo ID ----------------
 
@@ -178,7 +78,7 @@ function iniciar() {
 //         data.forEach(elemento =>{ elemento.UId
 //             maxId = elemento.UId
 //             console.log(maxId);
-        
+
 // // hago que ei ID sea 1 mayor y lo asigno a miImagen
 
 //             nombreImagen = "imagen" + (maxId + 1)
@@ -189,22 +89,7 @@ function iniciar() {
 
 //             miImagen.src = "https://firebasestorage.googleapis.com/v0/b/localstorage-e6721.appspot.com/o/imagenes%2Fbolsitas.jpg?alt=media&token=9c172991-c595-4817-82a5-589496d554e1"
 
-
-
-
-
-
-
-
-
-
 //         })
 // })
 
-
-
-
 // })
-
-
-
