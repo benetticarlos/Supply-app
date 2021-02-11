758b7c6d83091fb2af89d53419a2ec403ba9b9d2
CarlosBenetti <benetticarlos1@gmail.com>
2021-02-11 00:20:08 -0300

    algun cambio menor

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,21 +2,26 @@ var menuProductos = document.getElementById('menuProductos')
 var menuServicios = document.getElementById('menuServicios')
 var MenuTodos = document.getElementById8('MenuTodos')
 var contenido = document.getElementById('busquedasPublicadas')
+var cargaDinamica = document.getElementById('cargaDinamica')
+// function menuTodosDefault() {
+//     contenido.load("cuerpo.html")
+// }
 
-function menuTodosDefault() {
-    contenido.load("cuerpo.html")
-}
+// window.load = menuTodosDefault
 
-window.load = menuTodosDefault
+// MenuTodos.addEventListener('click', () =>{
+//     contenido.load("cuerpo.html")
+// } )
 
-MenuTodos.addEventListener('click', () =>{
-    contenido.load("cuerpo.html")
-} )
+// menuProductos.addEventListener('click', () =>{
+//     contenido.load("cuerpo_productos.html")
+// } )
 
-menuProductos.addEventListener('click', () =>{
-    contenido.load("cuerpo_productos.html")
-} )
+// menuServicios.addEventListener('click', () =>{
+//     contenido.load("cuerpo_servicios.html")
+// } )
 
-menuServicios.addEventListener('click', () =>{
-    contenido.load("cuerpo_servicios.html")
-} )
+menuServicios.addEventListener('click', ()=>{
+
+   console.log(cargaDinamica);
+})
